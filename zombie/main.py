d8f7bf1427b24b6262397f1890a081c6adc81c5a
sk367 <sirbubbles42@gmail.com>
2015-04-10 15:26:30 -0700

    added user login/out

--- zombie/main.py
+++ zombie/main.py
@@ -16,13 +16,31 @@
 #
 import webapp2
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         #self.response.write('Hello world!')
+        
+        user = users.get_current_user()
+        template_values = {}
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+            greeting = "Hello, "
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+            greeting = "Hello, you."
+
         template_values = {
-            'greetings': "Hello",
+          'greetings': greeting ,
+          'user': user,
+          'url': url,
+          'url_linktext': url_linktext
         }
+         
+        
         self.response.out.write(template.render("index.html", template_values))
     
 app = webapp2.WSGIApplication([
