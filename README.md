52eea0e4c225e1ed3f36c44c8f0b74f9dcdd4678
Nancy McCollough <nm389@nau.edu>
2015-04-10 09:55:21 -0700

    updated with some notes from class

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 to run google app engine: dev_appserver.py 
 more specific: dev_appserver.py zombie --port=8000
+Update schema article: https://cloud.google.com/appengine/articles/update_schema
--- zombie/hellomars.py
+++ zombie/hellomars.py
@@ -1,11 +1,21 @@
 #Using webapp2 framework...
 #from webapp2 import RequestHandler, SWGIApplication
 import webapp2
+import google.appengine.ext.ndb
+
+class MartianPizza(ndb.Model):
+	# localhost:8080/datastore to see
+	name = ndb.StringProperty(required=True)
+	toppings = ndb.StringProperty(repeated=True)
+	# Example: ["cheese", "pepperoni"]
+	microbes = ndb.IntegerProperty(default=10)
+	price = ndb.IntegerProperty(default=0)
 
 class SplashPage(webapp2.RequestHandler):
 	def get(self):
 		self.response.headers["Content-Type"] = 'text/plain'
 		self.response.write("Hello mars!")
+		pizza = MartianPizza(name="Red Rover Pizza", toppings=["cheese","red sauce"], price=500)
 
 app = webapp2.WSGIApplication([
 	('/', SplashPage),
