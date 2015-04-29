bec5fe94a529193811d7d9c2745545962d6467bc
Nancy McCollough <nm389@nau.edu>
2015-04-29 16:00:23 -0700

    profiles now show how many challenges the person has completed.
    changed jack's profile so it's different from Erin's.

--- zombie/app/handlers/profiles.py
+++ zombie/app/handlers/profiles.py
@@ -3,6 +3,7 @@
 import webapp2
 from google.appengine.api import users
 from app.models.models import *
+import app.models.models as models
 from jinja2 import Environment, PackageLoader
 
 env = Environment(loader=PackageLoader('app', 'templates'))
@@ -13,6 +14,7 @@ class Erin(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         template_values={}
+        challenges = len(models.ChallengesCompleted.query(models.ChallengesCompleted.username=='Erin').fetch())
         if user:
             url = users.create_logout_url('/')
             url_linktext = 'Logout'
@@ -25,7 +27,8 @@ class Erin(webapp2.RequestHandler):
           'greetings': greeting ,
           'user': user,
           'url': url,
-          'url_linktext': url_linktext
+          'url_linktext': url_linktext,
+          'challenges_completed': challenges
         }
         template = env.get_template("Erin.html")
         self.response.out.write(template.render(template_values))
@@ -35,6 +38,7 @@ class Jack(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         template_values={}
+        challenges = len(models.ChallengesCompleted.query(models.ChallengesCompleted.username=='Jack').fetch())
         if user:
             url = users.create_logout_url('/')
             url_linktext = 'Logout'
@@ -47,7 +51,8 @@ class Jack(webapp2.RequestHandler):
           'greetings': greeting ,
           'user': user,
           'url': url,
-          'url_linktext': url_linktext
+          'url_linktext': url_linktext,
+		  'challenges_completed': challenges
         }
         template = env.get_template("Jack.html")
         self.response.out.write(template.render(template_values))
